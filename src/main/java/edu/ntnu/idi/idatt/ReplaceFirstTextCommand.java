f9700f893a5c71dbd7537688904e061da600a884
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-05 14:13:49 +0100

    added to ReplaceFirstTextCommand

--- src/main/java/edu/ntnu/idi/idatt/ReplaceFirstTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/ReplaceFirstTextCommand.java
@@ -1,5 +1,13 @@
 package edu.ntnu.idi.idatt;
 
-public class ReplaceFirstTextCommand {
+public class ReplaceFirstTextCommand extends ReplaceTextCommand {
 
+  public ReplaceFirstTextCommand(String target, String replacement) {
+    super(target, replacement);
+  }
+
+  @Override
+  public String execute(String text) {
+    return text.replaceFirst(getTarget(), getReplacement());
+  }
 }

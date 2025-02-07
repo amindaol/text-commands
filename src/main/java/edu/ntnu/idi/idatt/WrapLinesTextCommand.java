4947a32aedb20c972ddecb6577315d6cc034b194
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-07 12:56:27 +0100

    Added to WrapLinesTextCommand

--- src/main/java/edu/ntnu/idi/idatt/WrapLinesTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/WrapLinesTextCommand.java
@@ -2,4 +2,24 @@ package edu.ntnu.idi.idatt;
 
 public class WrapLinesTextCommand {
 
+  private final String opening;
+  private final String end;
+
+  public WrapLinesTextCommand(String opening, String end) {
+    this.opening = opening;
+    this.end = end;
+  }
+
+  public String getOpening() {
+    return opening;
+  }
+
+  public String getEnd() {
+    return end;
+  }
+
+  @Override
+  public String execute(String text) {
+    return opening + text + end;
+  }
 }

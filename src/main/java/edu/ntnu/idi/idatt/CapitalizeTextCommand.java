edf0bb81c1b5781a19544c85cc2407b07bb0e11e
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-12 12:31:55 +0100

    Added to CapitalizeTextCommand class

--- src/main/java/edu/ntnu/idi/idatt/CapitalizeTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/CapitalizeTextCommand.java
@@ -1,5 +1,12 @@
 package edu.ntnu.idi.idatt;
 
-public class CapitalizeTextCommand {
+public class CapitalizeTextCommand implements TextCommand {
 
+  @Override
+  public String execute(String text) {
+    if (text == null || text.isEmpty()) {
+      return text;
+    }
+    return text.substring(0, 1).toUpperCase() + text.substring(1);
+  }
 }

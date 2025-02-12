b1ddf2179e1a54db08fd1fc0dbcc819a964872cc
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-12 12:44:31 +0100

    added to CapitalizeWordsTextCommand class

--- src/main/java/edu/ntnu/idi/idatt/CapitalizeWordsTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/CapitalizeWordsTextCommand.java
@@ -1,5 +1,18 @@
 package edu.ntnu.idi.idatt;
 
-public class CapitalizeWordsTextCommand {
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class CapitalizeWordsTextCommand extends CapitalizeTextCommand {
+
+  @Override
+  public String execute(String text) {
+    if (text == null || text.isEmpty()) {
+      return text;
+    }
+    return Arrays.stream(text.split("\\s+"))
+        .map(word -> word.substring(0, 1).toUpperCase() + word.substring(1))
+        .collect(Collectors.joining(" "));
+  }
 
 }

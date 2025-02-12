34c2b54bf8afb1d97798b898fab5d5dc34125429
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-12 13:13:04 +0100

    added to CapitalizeSelectionTextCommand

--- src/main/java/edu/ntnu/idi/idatt/CapitalizeSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/CapitalizeSelectionTextCommand.java
@@ -1,5 +1,23 @@
 package edu.ntnu.idi.idatt;
 
-public class CapitalizeSelectionTextCommand {
+public class CapitalizeSelectionTextCommand extends CapitalizeTextCommand {
 
+  private final String selection;
+
+  public CapitalizeSelectionTextCommand(String selection) {
+    this.selection = selection;
+  }
+
+  public String getSelection() {
+    return selection;
+  }
+
+  @Override
+  public String execute(String text) {
+    if (!text.contains(selection)) {
+      return text;
+    }
+    String capitalizedSelection = selection.substring(0, 1).toUpperCase() + selection.substring(1);
+    return text.replaceAll(selection, capitalizedSelection);
+  }
 }

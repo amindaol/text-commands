ce964d32f73db00cffeb74abfd8f70e0027cd0ae
Aminda Lunde <aminda.lunde@gmail.com>
2025-02-12 12:28:13 +0100

    Added to WrapSelectionTextCommand class

--- src/main/java/edu/ntnu/idi/idatt/WrapSelectionTextCommand.java
+++ src/main/java/edu/ntnu/idi/idatt/WrapSelectionTextCommand.java
@@ -1,5 +1,23 @@
 package edu.ntnu.idi.idatt;
 
-public class WrapSelectionTextCommand extends W {
+public class WrapSelectionTextCommand extends WrapTextCommand {
 
+  private final String selection;
+
+  public WrapSelectionTextCommand(String opening, String end, String selection) {
+    super(opening, end);
+    this.selection = selection;
+  }
+
+  public String getSelection() {
+    return selection;
+  }
+
+  @Override
+  public String execute(String text) {
+    if (!text.contains(selection)) {
+      return text;
+    }
+    return text.replaceFirst(selection, getOpening() + selection + getEnd());
+  }
 }
